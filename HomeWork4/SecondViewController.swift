cd26ae363d47c7c1b9a8ab8f71ff0fa9727ca1e4
ShakirKadirov <shakirkadirov1@gmail.com>
2024-01-19 00:31:50 +0600

    new code

---
+++ HomeWork4/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HomeWork4
+//
+//  Created by Shakir Kadirov on 18.01.2024.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    //
+    //
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
